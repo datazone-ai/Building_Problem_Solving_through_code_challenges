f4357eb6c390d62ba12ad7dc2ac28ada24269ff4
abiolaks <kingsleylawani@gmail.com>
2026-08-14 17:31:03 +0100

    complete task for day_1

---
+++ day_1/abiola_divide_or_square.py
@@ -0,0 +1,32 @@
+"""
+    Task
+
+Write a function called divide_or_square that takes one argument (a number):
+
+Returns the square root of the number if it is divisible by 5.
+Returns the remainder if it is not divisible by 5.
+
+Example: divide_or_square(10) → 3.16 (square root of 10)
+""""
+
+
+def divide_or_square(number: int | float) -> int | float:
+    if number % 5 == 0 :
+        return  round(number ** 0.5, 2)
+    else:
+        return number % 5
+
+
+
+# approach
+import math
+from typing import Union
+
+def divide_or_square(number: Union[int, float]) -> Union[int, float]:
+    if number % 5:
+        return round(math.sqrt(numbmer), 2)
+    else:
+        return number % 5
+
+
+assert divide_or_square(10) = 3.16
