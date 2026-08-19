d4460dabee0d52c5426b144ba7cd3eae5324757a
Sunmisola Barakat Lawal <lawalsunmisola2020@gmail.com>
2026-08-19 12:23:36 +0100

    completed day_2 task

---
+++ sunmisola_day_2_convert_add.py/convert_add.py
@@ -0,0 +1,11 @@
+def convert_add(string_list):
+    total = 0 
+    for string in string_list:
+        num = int(string)
+            
+    total = total + num
+    return total
+
+
+result = convert_add(["10","5","9","35","40"])
+print (result)
