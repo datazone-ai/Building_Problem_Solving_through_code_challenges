a829cd5e16d2740fc51f626b3ef594309d0d8a64
BelloOlawale <bello.o.olawale@gmail.com>
2026-08-27 14:59:07 +0100

    completed day 2 task and corrected day 1 task

--- day_1/olawale_divide_or_square.py
+++ day_1/olawale_divide_or_square.py
@@ -1,12 +1,8 @@
-
 import math
 from typing import Union
-def divide_or_square(number: int | float)-> int | float: 
-    
+
+def divide_or_square(number: int | float) -> int | float:
     if number % 5 == 0:
         return round(math.sqrt(number), 2)
     else:
         return number % 5
-    
-assert divide_or_square(10) == 3.16
-assert divide_or_square(8) == 3
